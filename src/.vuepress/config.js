b38653c1e64806b204056c95a91bcd3da95b7136
AyalaKaguya <ayalakaguya@outlook.com>
2023-04-02 13:10:13 +0800

    init vuepress

---
+++ src/.vuepress/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	dest: "./public",
+	temp: "./node_modules/.temp/theme",
+
+	title: "EasyBangumi",
+	description: "Free and open source bangumi(animation) player for Android",
+  }
