e482077ba0045b43dc14e3bddefbad3e19bb1431
AyalaKaguya <ayalakaguya@outlook.com>
2023-12-16 23:08:39 +0800

    fix：部分文档因修改语言导致的路径错误

---
+++ src/.vuepress/theme/components/Page.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import PageMeta from './PageMeta.vue'
+import PageNav from './PageNav.vue'
+
+defineSlots<{
+  'top'?: (props: Record<never, never>) => any
+  'bottom'?: (props: Record<never, never>) => any
+  'content-top'?: (props: Record<never, never>) => any
+  'content-bottom'?: (props: Record<never, never>) => any
+}>()
+</script>
+
+<template>
+  <main class="page">
+    <slot name="top" />
+
+    <div class="theme-default-content">
+      <slot name="content-top" />
+
+      <Content />
+
+      <slot name="content-bottom" />
+    </div>
+
+    <PageMeta />
+
+    <PageNav />
+
+    <slot name="bottom" />
+  </main>
+</template>
