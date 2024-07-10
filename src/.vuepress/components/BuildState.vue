e7ff3ea6b0c7a9b515951596ff4f85cf0bdcfb03
AyalaKaguya <ayalakaguya@outlook.com>
2024-07-10 22:56:31 +0800

    doc: 优化主页在桌面端的呈现

---
+++ src/.vuepress/components/BuildState.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="shields-wrapper">
+        <img alt="release" src="https://img.shields.io/github/v/release/easybangumiorg/EasyBangumi" />
+        <img alt="license" src="https://img.shields.io/github/license/easybangumiorg/EasyBangumi" />
+        <img alt="downloads" src="https://img.shields.io/github/downloads/easybangumiorg/EasyBangumi/total" />
+        <img alt="issues" src="https://img.shields.io/github/issues/easybangumiorg/EasyBangumi" />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.shields-wrapper {
+    text-align: center;
+
+    img {
+        display: inline-block;
+        margin-left: 0;
+        margin-right: 0.25rem;
+        margin-top: 0.25rem;
+        margin-bottom: 0.25rem;
+        width: auto;
+        height: 1.5rem !important;
+    }
+}
+</style>
